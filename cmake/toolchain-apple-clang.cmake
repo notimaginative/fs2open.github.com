b24cd1623e32594fc18b62784aff4d1575ba673f
Taylor Richards <taylor@notimaginative.com>
2022-12-09 17:58:36 -0500

    use proper optimization flags on macOS

--- cmake/toolchain-apple-clang.cmake
+++ cmake/toolchain-apple-clang.cmake
@@ -13,6 +13,6 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated -Wno-char-subscripts")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcolor-diagnostics")
 
-set(CMAKE_CXX_FLAGS_RELEASE "-O2 -Wno-unused-variable -Wno-unused-parameter")
+set(CMAKE_CXX_FLAGS_RELEASE "-O2 -Wno-unused-variable")
 
-set(CMAKE_CXX_FLAGS_DEBUG "-Og -g -Wshadow")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g -Wextra -Wshadow -Wno-unused-parameter")
